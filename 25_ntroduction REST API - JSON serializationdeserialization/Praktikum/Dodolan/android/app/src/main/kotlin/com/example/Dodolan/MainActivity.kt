01f279ca72b663585307f4c179f99c5635591421
Sadamm3m <111202012765@mhs.dinus.ac.id>
2023-06-27 00:47:29 +0700

    baru

---
+++ 25_ntroduction REST API - JSON serializationdeserialization/Praktikum/Dodolan/android/app/src/main/kotlin/com/example/Dodolan/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.example.Dodolan
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ 25_ntroduction REST API - JSON serializationdeserialization/Praktikum/Dodolan/lib/view/init_screen/splash.dart
@@ -0,0 +1,45 @@
+import 'package:Dodolan/imports.dart';
+import 'dart:developer' as dev;
+
+class Splash extends StatefulWidget {
+  @override
+  _SplashState createState() => _SplashState();
+}
+
+class _SplashState extends State<Splash> {
+  @override
+  void initState() {
+    super.initState();
+    dev.log('Splash Init');
+    Future.delayed(Duration(seconds: 2)).then((value) => getData());
+  }
+
+  getData() {
+    Navigator.pushAndRemoveUntil(
+        context, MaterialPageRoute(builder: (_) => Login()), (route) => false);
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return SafeArea(
+        child: Scaffold(
+      body: Center(
+        child: Row(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            TextBuilder(
+              text: 'DODO',
+              color: Constraints.APP_BAR_HOME_TEXT1_COLOR,
+              fontSize: 30.0,
+            ),
+            TextBuilder(
+              text: 'LAN',
+              color: Constraints.APP_BAR_HOME_TEXT2_COLOR,
+              fontSize: 30.0,
+            ),
+          ],
+        ),
+      ),
+    ));
+  }
+}
